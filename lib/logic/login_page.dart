86fac4bcf7d6fdd64eb53958da38f8971fc25340
Sandev <sandevdewthilina2000@gmail.com>
2020-11-15 19:53:47 +0530

    internet permission added

--- lib/logic/login_page.dart
+++ lib/logic/login_page.dart
@@ -1,4 +1,4 @@
-import 'package:auto_size_text/auto_size_text.dart';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hrms_supervisor_app/constants/runtime_constants.dart';
